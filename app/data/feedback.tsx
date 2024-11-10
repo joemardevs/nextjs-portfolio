c17b68bea80bd8284fd497c6404615e95b0964a6
Joemar Palting <joemarpalting19@gmail.com>
2024-11-10 23:12:42 +0800

    fixed: content

--- app/data/feedback.tsx
+++ app/data/feedback.tsx
@@ -4,14 +4,14 @@ export const FEEDBACK = [
     position: "Client",
     featured: false,
     feedback:
-      "He is truly an expert in his field, consistently delivering remarkable outputs that meet and often exceed his clients&apos; expectations. His commitment to quality service is evident in his willingness to ensure a smooth delivery process. He is a good listener, attentive to his clients&apos; needs, and meticulously follows their instructions to achieve the desired results. We didn&apos;t find it difficult to talk to him and share our ideas and suggestions because he is approachable and not intimidating. Regarding the quality of his products, I can attest to his exceptional work ethic and skills. The gamified website he created was not only well-designed but also highly engaging and enjoyable to use. His dedication to his craft and attention to detail make him an outstanding professional, capable of delivering exceptional value to his clients.",
+      "He is truly an expert in his field, consistently delivering remarkable outputs that meet and often exceed his clients' expectations. His commitment to quality service is evident in his willingness to ensure a smooth delivery process. He is a good listener, attentive to his clients' needs, and meticulously follows their instructions to achieve the desired results. We didn't find it difficult to talk to him and share our ideas and suggestions because he is approachable and not intimidating. Regarding the quality of his products, I can attest to his exceptional work ethic and skills. The gamified website he created was not only well-designed but also highly engaging and enjoyable to use. His dedication to his craft and attention to detail make him an outstanding professional, capable of delivering exceptional value to his clients.",
   },
   {
     name: "Claudine Marie P. Jose",
     position: "Client",
     featured: false,
     feedback:
-      "I want to extend my heartfelt thanks to Kuya Joemar on developing our website-based gamified application.  The application is incredibly engaging and easy to use. It&apos;s clear that he put a lot of thought and effort into the design and functionality. The gamified elements blend in perfectly, making the experience both fun and educational.",
+      "I want to extend my heartfelt thanks to Kuya Joemar on developing our website-based gamified application.  The application is incredibly engaging and easy to use. It's clear that he put a lot of thought and effort into the design and functionality. The gamified elements blend in perfectly, making the experience both fun and educational.",
   },
   {
     name: "Nyca Perpetua V. Labuguen",
@@ -25,21 +25,21 @@ export const FEEDBACK = [
     position: "Client",
     featured: false,
     feedback:
-      "I particularly appreciate the responsiveness of the IT developer to solve problems/difficulties with the application and his level of communication through this development effort. Likewise, it has been notable how the features and functionality he implemented align with our research needs. The developed application wouldn&apos;t come into reality without his expertise.",
+      "I particularly appreciate the responsiveness of the IT developer to solve problems/difficulties with the application and his level of communication through this development effort. Likewise, it has been notable how the features and functionality he implemented align with our research needs. The developed application wouldn't come into reality without his expertise.",
   },
   {
     name: "Daryll Lorenzo",
     position: "Client",
     featured: true,
     feedback:
-      "He&apos;s an excellent developer who is also quite accommodating. Communicating with him regarding the project was straightforward.",
+      "He's an excellent developer who is also quite accommodating. Communicating with him regarding the project was straightforward.",
   },
   {
     name: "Angelo Padilla",
     position: "Client",
     featured: false,
     feedback:
-      "I want to commend Joemar for his exceptional ability to actively listen and provide valuable suggestions. His attentive and empathetic approach to listening creates a comfortable space for effective communication. What sets Joemar apart is not just his ability to understand, but also his skill in offering thoughtful and insightful suggestions. Whether it&apos;s lending a compassionate ear or providing constructive advice, Joemar consistently demonstrates a commendable knack for fostering positive and helpful conversations. His talent in both listening and offering meaningful suggestions is truly commendable and greatly appreciated.",
+      "I want to commend Joemar for his exceptional ability to actively listen and provide valuable suggestions. His attentive and empathetic approach to listening creates a comfortable space for effective communication. What sets Joemar apart is not just his ability to understand, but also his skill in offering thoughtful and insightful suggestions. Whether it's lending a compassionate ear or providing constructive advice, Joemar consistently demonstrates a commendable knack for fostering positive and helpful conversations. His talent in both listening and offering meaningful suggestions is truly commendable and greatly appreciated.",
   },
   {
     name: "Klaude Mendoza",
@@ -53,13 +53,13 @@ export const FEEDBACK = [
     position: "Client",
     featured: true,
     feedback:
-      "Accommodating developer. Can do works on time. Easy to communicate when we&apos;re asking about the system.",
+      "Accommodating developer. Can do works on time. Easy to communicate when we're asking about the system.",
   },
   {
     name: "Isabel Agsunod",
     position: "Client",
     featured: true,
     feedback:
-      "Not only he&apos;s a skilled developer, but he can also discuss project-related issues and is an excellent listener.",
+      "Not only he's a skilled developer, but he can also discuss project-related issues and is an excellent listener.",
   },
 ];
