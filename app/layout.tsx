78fe99221000bb4f32ba8aeed0b384b66007e8a6
Joemar Palting <joemarpalting19@gmail.com>
2024-12-13 09:04:44 +0800

    Added speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ThemeProvider from "./components/ThemeProvider";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Joemar Palting - Software Engineer",
@@ -37,6 +38,7 @@ export default function RootLayout({
         </div>
       </div>
       <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
   );
 }
